066dc3616f705ae2802a2999e5321a44bd4c9130
lujanfernaud <lujanfernaud@gmail.com>
2016-11-07 08:51:20 +0000

    Add possibility to see only a particular year or month

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -11,8 +11,35 @@ class ArchiveController < ApplicationController
   end
 
   def show
-    @articles_by_month = Article.all.group_by { |article| article.created_at.strftime("%B") }
-    @month = params[:id]
-    @articles = @articles_by_month[@month]
+
+    if params[:id].length > 5
+      @month = params[:id][5..-1]
+      @sort_by_month = true
+    else
+      @year = params[:id]
+      @sort_by_year = true
+    end
+
+    if @month
+      @year = params[:id][0..3]
+
+      @articles_by_year = Article.all.group_by { |article| article.created_at.strftime("%Y") }
+
+      @articles_by_year.each do |year, month|
+        @articles_by_year[year] = month.group_by { |article| article.created_at.strftime("%B") }
+      end
+
+      @articles_by_year_and_month = @articles_by_year[@year]
+      @articles_by_month = @articles_by_year_and_month[@month]
+
+    else
+      @articles_by_year = Article.all.group_by { |article| article.created_at.strftime("%Y") }
+
+      @articles_by_year.each do |year, month|
+        @articles_by_year[year] = month.group_by { |article| article.created_at.strftime("%B") }
+      end
+
+      @articles_by_year_and_month = @articles_by_year[@year]
+    end
   end
 end
