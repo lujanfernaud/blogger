4bec0eb6f25c6d60c44ec65a878657e70d7e8479
lujanfernaud <lujanfernaud@gmail.com>
2016-11-08 06:35:12 +0000

    Move logic to helper and refactor

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -1,45 +1,26 @@
 class ArchiveController < ApplicationController
+  include ArchiveHelper
 
   def index
-    @articles_by_year = Article.all.group_by { |article| article.created_at.strftime("%Y") }
-
-    @articles_by_year.each do |year, month|
-      @articles_by_year[year] = month.group_by { |article| article.created_at.strftime("%B") }
-    end
-
-    @articles_by_year_and_month = @articles_by_year
+    @articles_by_year_and_month = group_articles_by_year_and_month
   end
 
   def show
 
     if params[:id].length > 5
-      @month = params[:id][5..-1]
+      @month         = params[:id][5..-1]
       @sort_by_month = true
     else
-      @year = params[:id]
-      @sort_by_year = true
+      @year          = params[:id]
+      @sort_by_year  = true
     end
 
-    if @month
+    if @sort_by_month
       @year = params[:id][0..3]
-
-      @articles_by_year = Article.all.group_by { |article| article.created_at.strftime("%Y") }
-
-      @articles_by_year.each do |year, month|
-        @articles_by_year[year] = month.group_by { |article| article.created_at.strftime("%B") }
-      end
-
-      @articles_by_year_and_month = @articles_by_year[@year]
-      @articles_by_month = @articles_by_year_and_month[@month]
-
+      @articles_by_year_and_month = group_articles_by_year_and_month[@year]
+      @articles_by_month          = @articles_by_year_and_month[@month]
     else
-      @articles_by_year = Article.all.group_by { |article| article.created_at.strftime("%Y") }
-
-      @articles_by_year.each do |year, month|
-        @articles_by_year[year] = month.group_by { |article| article.created_at.strftime("%B") }
-      end
-
-      @articles_by_year_and_month = @articles_by_year[@year]
+      @articles_by_year_and_month = group_articles_by_year_and_month[@year]
     end
   end
 end
--- app/helpers/archive_helper.rb
+++ app/helpers/archive_helper.rb
@@ -1,2 +1,10 @@
 module ArchiveHelper
+
+  def group_articles_by_year_and_month
+    articles_by_year = Article.all.group_by { |article| article.created_at.strftime("%Y") }
+
+    articles_by_year.each do |year, month|
+      articles_by_year[year] = month.group_by { |article| article.created_at.strftime("%B") }
+    end
+  end
 end
