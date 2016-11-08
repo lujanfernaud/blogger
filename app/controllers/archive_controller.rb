753d4314dcfddf0681097ece2841f46ddbf3306b
lujanfernaud <lujanfernaud@gmail.com>
2016-11-08 07:00:44 +0000

    Move logic to helper

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -6,16 +6,9 @@ class ArchiveController < ApplicationController
   end
 
   def show
+    check_if_year_or_month
 
-    if params[:id].length > 5
-      @month         = params[:id][5..-1]
-      @sort_by_month = true
-    else
-      @year          = params[:id]
-      @sort_by_year  = true
-    end
-
-    if @sort_by_month
+    if @month
       @year = params[:id][0..3]
       @articles_by_year_and_month = group_articles_by_year_and_month[@year]
       @articles_by_month          = @articles_by_year_and_month[@month]
--- app/helpers/archive_helper.rb
+++ app/helpers/archive_helper.rb
@@ -1,5 +1,15 @@
 module ArchiveHelper
 
+  def check_if_year_or_month
+    if params[:id].length > 5
+      @month         = params[:id][5..-1]
+      @sort_by_month = true
+    else
+      @year          = params[:id]
+      @sort_by_year  = true
+    end
+  end
+
   def group_articles_by_year_and_month
     articles_by_year = Article.all.group_by { |article| article.created_at.strftime("%Y") }
 
