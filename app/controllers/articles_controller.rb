8fc7ccf38d7a00c7f6b5f80d12274ed74f1c76b9
lujanfernaud <lujanfernaud@gmail.com>
2016-11-26 09:12:42 +0000

    Limit previous articles in index to 7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
 
   def index
     @latest_article = Article.last
-    @articles = Article.all.reverse[1..10]
+    @articles = Article.all.reverse[1..7]
   end
 
   def show
