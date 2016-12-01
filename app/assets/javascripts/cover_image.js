ba05923f8d63f264d014d53f6481b86f841a2910
lujanfernaud <lujanfernaud@gmail.com>
2016-12-01 19:11:00 +0000

    Move logic into adjustHeader()

--- app/assets/javascripts/cover_image.js
+++ app/assets/javascripts/cover_image.js
@@ -1,19 +1,23 @@
-$(document).on('turbolinks:load', function() {
+function adjustHeader() {
   $(".post-header").css("height", window.innerHeight);
 
   var headerPadding = parseInt($(".intro-header").css("padding-top"));
   var headingHeight = $(".post-heading").outerHeight();
-
   $(".post-heading").css("top", window.innerHeight - (headingHeight + headerPadding));
+
+  var postHeadingTop = parseInt($(".post-heading").css("top"));
+  var faHeight = $(".row-fa").outerHeight();
+  var fontSize = parseInt($("body").css("font-size"));
+  $(".row-fa").css("top", postHeadingTop - faHeight - fontSize);
+};
+
+$(document).on('turbolinks:load', function() {
+  adjustHeader();
 });
 
 $(function () {
   $(window).on("load resize", function () {
-    $(".post-header").css("height", window.innerHeight);
 
-    var headerPadding = parseInt($(".intro-header").css("padding-top"));
-    var headingHeight = $(".post-heading").outerHeight();
-
-    $(".post-heading").css("top", window.innerHeight - (headingHeight + headerPadding));
+  adjustHeader();
   });
 });
