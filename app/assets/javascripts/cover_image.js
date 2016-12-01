6b1535fef4373547d4952b29aeccecaabda96b2c
lujanfernaud <lujanfernaud@gmail.com>
2016-12-01 08:23:06 +0000

    Properly set post-heading position and move hr

--- app/assets/javascripts/cover_image.js
+++ app/assets/javascripts/cover_image.js
@@ -1,11 +1,19 @@
 $(document).on('turbolinks:load', function() {
   $(".post-header").css("height", window.innerHeight);
-  $(".post-heading").css("top", window.innerHeight - 313);
+
+  var headerPadding = parseInt($(".intro-header").css("padding-top"));
+  var headingHeight = $(".post-heading").outerHeight();
+
+  $(".post-heading").css("top", window.innerHeight - (headingHeight + headerPadding));
 });
 
 $(function () {
   $(window).on("load resize", function () {
     $(".post-header").css("height", window.innerHeight);
-    $(".post-heading").css("top", window.innerHeight - 313);
+
+    var headerPadding = parseInt($(".intro-header").css("padding-top"));
+    var headingHeight = $(".post-heading").outerHeight();
+
+    $(".post-heading").css("top", window.innerHeight - (headingHeight + headerPadding));
   });
 });
