aae7c6fce380348afba06b11d9f9bfdb7ed1627a
lujanfernaud <lujanfernaud@gmail.com>
2016-12-06 08:25:29 +0000

    Change Ckeditor configuration

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,32 @@
+CKEDITOR.editorConfig = function(config) {
+  config.language = 'en';
+  config.uiColor = '#F7B42C';
+  config.height = 300;
+  config.toolbarCanCollapse = true;
+  
+  config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+
+  config.toolbarGroups = [
+    { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+    { name: 'editing', groups: [ 'find', 'selection', 'editing' ] },
+    { name: 'forms', groups: [ 'forms' ] },
+    { name: 'tools', groups: [ 'tools' ] },
+    '/',
+    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+    { name: 'styles', groups: [ 'styles' ] },
+    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+    { name: 'links', groups: [ 'links' ] },
+    { name: 'insert', groups: [ 'insert' ] },
+    { name: 'colors', groups: [ 'colors' ] },
+
+    { name: 'others', groups: [ 'others' ] },
+    { name: 'about', groups: [ 'about' ] }
+  ];
+
+  config.removeButtons = 'Save,NewPage,Print,Templates,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,BidiLtr,BidiRtl,Flash,Table,Smiley,Iframe,TextColor,BGColor,FontSize,Font,Anchor,Subscript,Superscript,CopyFormatting,CreateDiv,Language,About,Cut,Copy,Paste,Scayt,SelectAll,HorizontalRule,PageBreak';
+};
