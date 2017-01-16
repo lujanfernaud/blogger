8665f4adb7f772f1415efc198a843ea82867224a
lujanfernaud <lujanfernaud@gmail.com>
2017-01-16 08:27:41 +0000

    Add condition for smooth scrolling on small devices

--- app/assets/javascripts/smooth_scroll.js
+++ app/assets/javascripts/smooth_scroll.js
@@ -1,13 +1,17 @@
 function smoothScroll() {
 
-    var headingHeight = $(".post-heading").outerHeight();
-    var faHeight = $(".row-fa").outerHeight();
-    var fontSize = parseInt($("body").css("font-size"));
+  $('a[href*="#"]:not([href="#"])').click(function() {
 
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+      if (screen.width > 767) {
+
+        var headingHeight = $(".post-heading").outerHeight();
+        var faHeight      = $(".row-fa").outerHeight();
+        var fontSize      = parseInt($("body").css("font-size"));
 
         if (target.length) {
           $('html, body').animate({
@@ -15,9 +19,22 @@ function smoothScroll() {
           }, 1000);
           return false;
         }
-      }
-    });
-  };
+
+      } else {
+
+        var headingHeight = $(".post-heading-xs").outerHeight();
+
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - headingHeight
+          }, 1000);
+          return false;
+        }
+
+      };
+    };
+  });
+};
 
 $(document).on('turbolinks:load', function() {
   smoothScroll();
