7cb56bd7f1ca57c91b2f7b7cd177971b8ef479c1
lujanfernaud <lujanfernaud@gmail.com>
2017-01-17 07:21:21 +0000

    Refactor, add guard clause

--- app/assets/javascripts/smooth_scroll.js
+++ app/assets/javascripts/smooth_scroll.js
@@ -2,18 +2,19 @@ function smoothScroll() {
 
   $('a[href*="#"]:not([href="#"])').click(function() {
 
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
       function stopAnimationIfUserScrolls() {
         $('html, body').on("scroll mousedown DOMMouseScroll mousewheel touchstart keyup", function(e) {
           if ( e.which > 0 || e.type === "mousedown" || e.type === "mousewheel" || e.type === "touchstart") {
             $('html, body').stop();
-          }
+          };
         });
       };
 
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
       var target = $(this.hash);
       target     = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (!target.length) { throw new Error("Section id not found") }
 
       if (screen.width > 767) {
 
@@ -21,25 +22,21 @@ function smoothScroll() {
         var faHeight      = $(".row-fa").outerHeight();
         var fontSize      = parseInt($("body").css("font-size"));
 
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top - (headingHeight + faHeight + (fontSize * 4))
-          }, 1000);
-          stopAnimationIfUserScrolls();
-          return false;
-        }
+        $('html, body').animate({
+          scrollTop: target.offset().top - (headingHeight + faHeight + (fontSize * 4))
+        }, 1000);
+        stopAnimationIfUserScrolls();
+        return false;
 
       } else {
 
         var headingHeight = $(".post-heading-xs").outerHeight();
 
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top - headingHeight
-          }, 1000);
-          stopAnimationIfUserScrolls();
-          return false;
-        }
+        $('html, body').animate({
+          scrollTop: target.offset().top - (headingHeight - 1)
+        }, 1000);
+        stopAnimationIfUserScrolls();
+        return false;
 
       };
     };
