fde25c144f394146cd05684c664df3b1ae02d6ee
lujanfernaud <lujanfernaud@gmail.com>
2017-01-21 18:05:18 +0000

    Add semicolons to return statements

--- app/assets/javascripts/header_opacity.js
+++ app/assets/javascripts/header_opacity.js
@@ -1,11 +1,11 @@
 function changeHeaderOpacity() {
-  if (screen.width < 767) { return };
+  if (screen.width < 767) { return; };
 
   var header       = $(".cover-image");
   var headerHeight = header.outerHeight();
 
   $(document).scroll(function() {
-    if (window.scrollY > headerHeight) { return };
+    if (window.scrollY > headerHeight) { return; };
 
     var scrollPercent = (headerHeight - window.scrollY) / headerHeight;
 
