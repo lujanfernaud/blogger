00f8d65af0c6651c84fd401083a0cc7fbad264a9
lujanfernaud <lujanfernaud@gmail.com>
2017-01-26 07:45:25 +0000

    Add demo_user?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :demo_user
+  helper_method :demo_user, :demo_user?
 
   def demo_user
     @demo_user = Author.find(4)
+  end
+
+  def demo_user?
+    current_user == demo_user
   end
 end
