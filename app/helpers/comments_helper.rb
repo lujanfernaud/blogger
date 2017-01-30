9b2ef917d8a8d933124629fb4c5843db15c11228
lujanfernaud <lujanfernaud@gmail.com>
2017-01-30 22:17:00 +0000

    Add method to find comment number in article

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,8 @@
 module CommentsHelper
+  def comment_number(comment)
+    article_comments_array = @article.comments.sort
+    # Send article comments to a hash, with the index as a value.
+    article_comments_hash = Hash[article_comments_array.map.with_index(1).to_a]
+    article_comments_hash[comment]
+  end
 end
