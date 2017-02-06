f6b6743d50601c7fa2de1bd65538c47fb9a419b7
lujanfernaud <lujanfernaud@gmail.com>
2017-02-06 07:25:21 +0000

    Sort articles by creation date before grouping them

--- app/helpers/archive_helper.rb
+++ app/helpers/archive_helper.rb
@@ -11,7 +11,8 @@ module ArchiveHelper
   end
 
   def group_articles_by_year_and_month
-    articles_by_year = Article.all.reverse.group_by { |article| article.created_at.strftime("%Y") }
+    sorted_articles  = Article.all.sort_by { |article| article.created_at }
+    articles_by_year = sorted_articles.reverse.group_by { |article| article.created_at.strftime("%Y") }
 
     articles_by_year.each do |year, month|
       articles_by_year[year] = month.group_by { |article| article.created_at.strftime("%B") }
